769d11c7e1758b07e6b246e8f6c9e50c622a5e5c
tobnys <tobnystrom@gmail.com>
2017-11-16 04:22:14 +0100

    JS scroll functionality added

--- public/client.js
+++ public/client.js
@@ -1,10 +1,19 @@
 $(document).scroll(function(){
+    // Shrink navbar on scrolldown.
     if($(this).scrollTop() > 30){
         $(".main-nav").addClass("down");
         $(".main-nav a").addClass("text-down");
     }
+    // Enlarge navbar on scrollup.
     else {
         $(".main-nav").removeClass("down");
         $(".main-nav a").removeClass("text-down");
+    }
+
+    // Show elements on scrolldown.
+    if($(this).scrollTop() > 150){
+        $(".about-page p, .about-page h2, .sub-line").animate({
+             opacity: 1,
+        }, 1000);
     }
 });
