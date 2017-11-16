85d4a0b52e16fbf7fd40fbc6afcc7a21f0d728ce
tobnys <tobnystrom@gmail.com>
2017-11-17 00:48:38 +0100

    Smooth scrolling added and fully working

--- public/client.js
+++ public/client.js
@@ -1,4 +1,50 @@
+$(function (){
+    var mainRoot = $('html, body');
+
+    // Smooth scrolling navigation
+    $("#page-home, #page-about, #page-skills, #page-projects, #page-contact").click(function(e){
+        if(e.target.id === "page-home"){
+            mainRoot.animate({
+                scrollTop: 0
+            }, 800, function(e){
+                window.location.hash = "home";
+            });
+        }   
+        if(e.target.id === "page-about"){
+            mainRoot.animate({
+                scrollTop: 920
+            }, 800, function(e){
+                window.location.hash = "about";
+            });
+        }
+        if(e.target.id === "page-skills"){
+            mainRoot.animate({
+                scrollTop: 1310
+            }, 800, function(e){
+                window.location.hash = "skills";
+            });
+        }
+        if(e.target.id === "page-projects"){
+            mainRoot.animate({
+                scrollTop: 2478
+            }, 800, function(e){
+                window.location.hash = "projects";
+            });
+        }
+        if(e.target.id === "page-contact"){
+            mainRoot.animate({
+                scrollTop: 2862
+            }, 800, function(e){
+                window.location.hash = "contact";
+            });
+        }
+    })
+});
+
+
+// Target scrolling
 $(document).scroll(function(){
+    console.log($(this).scrollTop());
     // Shrink navbar on scrolldown.
     if($(this).scrollTop() > 30){
         $(".main-nav").addClass("down");
@@ -17,6 +63,3 @@ $(document).scroll(function(){
         }, 1000);
     }
 });
-
-
-
